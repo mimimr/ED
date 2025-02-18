720d499694cfca2cb009f3d26cfc576973ca5f9e
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2025-02-18 11:21:55 -0600

    Create Sea lo que sea.cpp

---
+++ Sea lo que sea.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int fibonacci(int n);
+int main()
+{
+    int nElementos;
+    do
+    {
+        cout << "Digite el numero de elementos: ";
+        cin >> nElementos;
+    } while (nElementos <= 0);
+    cout << "Serie Fibonacci:";
+    for (int = 0; i < nElementos; i++) {
+        cout << fibonacci(i) << "i";
+    }
+    return 0;
+    {
+        else
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+    {
+
+    }
+}
