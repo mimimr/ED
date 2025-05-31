6c5b01af4a91bc0c2f1db9aa34bac9d9a296837f
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2025-05-31 15:55:36 -0600

    Update Velocidad.cpp

--- Velocidad.cpp
+++ Velocidad.cpp
@@ -1,39 +1,41 @@
 //Ejercicio 1 / Practica 1
 
 #include <iostream>
-using namespace std; // entada / salida cin - cout
+using namespace std; // entrada/salida cin - cout
 
 float pedirDistancia();
 float pedirTiempo();
 float calcularVelocidad(float distancia, float tiempo);
-void mostrarVelocidad(float velocidad); // se usa void cuand quiero declarar una variable NO calcular o realizar una opercacion
+void mostrarVelocidad(float velocidad); // se usa void cuando quiero declarar una variable NO calcular o realizar una operación
 
 int main()
-{ 
-	float distancia = pedirDistancia();
-	float tiempo = pedirTiempo();
-	float velocidad = calcularVelocidad(distancia, tiempo);
-	mostrarVelocidad(velocidad);
-	return 0;
+{
+    float distancia = pedirDistancia();
+    float tiempo = pedirTiempo();
+    float velocidad = calcularVelocidad(distancia, tiempo);
+    mostrarVelocidad(velocidad);
+    return 0;
 }
 
 float pedirDistancia() {
-	float d;
-	cout << "Distancia en metros: ";
-	cin >> d;
-	return d;
+    float d;
+    cout << "Distancia en metros: ";
+    cin >> d;
+    return d;
 }
+
 float pedirTiempo() {
-	float t;
-	cout << "Tiempo en segundos";
-	cin >> t;
-	return t;
+    float t;
+    cout << "Tiempo en segundos: ";
+    cin >> t;
+    return t;
 }
+
 float calcularVelocidad(float distancia, float tiempo) {
-
-
+    // Velocidad = Distancia / Tiempo
+    return distancia / tiempo;
 }
+
 void mostrarVelocidad(float velocidad) {
-
-
+    cout << "La velocidad es: " << velocidad << " m/s" << endl;
 }
