b2257a1266cc9c98bab541d060365e8e810193b2
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2025-06-06 12:09:32 -0600

    Update ABC.cpp

--- ABC.cpp
+++ ABC.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string.h>
 using namespace std;
@@ -10,34 +9,37 @@ char palabraUsuario[30];
 int main()
 {
 	pedirDatos();
-	//Llamada a la funcion para contar vocale del nombre
+	//Llamada a la funcion para contar vocales del nombre
 	contarVocales(palabraUsuario);
 }
-void pedirDatos();
+void pedirDatos()
 {
-	cout << "Digite una palabra:";
+	cout << "Digite una palabra: ";
 	cin.getline(palabraUsuario, 30, '\n');
 }
-void contarVocales(char* palabra)
+void contarVocales(char*palabra)
 {
-	int contA = 0, cont = 0, contI = 0, contO = 0, contU = 0;
-	//ientras nombre sea diferente de nulo '\0'
+	int contA = 0, contE = 0, contI = 0, contO = 0, contU = 0;
+	// mientras nombre sea diferente de nulo '\0'
 	while (*palabra)
 	{
 		switch (*palabra)
 		{
-		case 'A': contA++; break;
-		case 'E': contE++; break;
-		case 'I': contI++; break;
-		case 'O': contO++; break;
-		case 'U': contU++; break;
+		case'A':contA++; break;
+		case'E':contE++; break;
+		case'I':contI++; break;
+		case'O':contO++; break;
+		case'U':contU++; break;
+		
 		}
 		palabra++;
 	}
-	//Imprimiendo el conteo
-	cout << "\n Numero de vocales A;" << contA << endl;
-	cout << "\n Numero de vocales E;" << contE << endl;
-	cout << "\n Numero de vocales I;" << contI << endl;
-	cout << "\n Numero de vocales O;" << contO << endl;
-	cout << "\n Numero de vocales U;" << contU << endl;
+	
+	//Imprimir el conteo de cada vocal
+	cout << "\n Numero de vocales A" << contA << endl;
+	cout << "\n Numero de vocales E" << contE << endl;
+	cout << "\n Numero de vocales I" << contI << endl;
+	cout << "\n Numero de vocales O" << contO << endl;
+	cout << "\n Numero de vocales U" << contU << endl;
 }
+
