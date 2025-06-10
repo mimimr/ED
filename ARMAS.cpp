985702c43db7b2d80f014aff2d472a88492cbfb7
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2025-06-10 12:29:57 -0600

    Update ARMAS.cpp

--- ARMAS.cpp
+++ ARMAS.cpp
@@ -1,75 +1,51 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
+
 //La estructura :D
-struct arma {
+struct Arma {
     string nombre;
     int precio;
-    int danio;
+    int dano;
 };
 int main()
 {
     //Poner armas disponibles
-    arma armas[] = {
+    int opcion;
+    Arma armas[3] = {
         {"Espada oxidada",10,13},
         {"Escudo magico",20,17},
         {"Arco elfico",15,15}
     };
     //Numero de armas disponibles
-    int cantidadArmas = sizeof(armas) / sizeof(arma);
+    int cantidadArmas = sizeof(armas) / sizeof(Arma);
 
-    //Oro del jugador
-    int oroJugador = 30;
-    int* oroActual = &oroJugador; //*& Son los punteros
+    // Cantidad de oro que tiene el jugador
+    int oroJugador = 60;
+    int* oroActual = &oroJugador; // Se indica donde se encuentra la cantidad del dinero
 
-    while (true) {//Bucle principal
-        cout << "Bienvenido a la tienda de Doña Pelos" << endl;
-        cout << "Oro disponible: " << *oroActual << "Moneditas" << endl;
 
-        //Armas disponibles
-        arma* armasEnVenta = armas;
-        cout << "Chequese lo que tenemos: " << endl;
+    //Implementar bucle usando while
+    while (true) {
+        cout << "Bienvenido a la tienda de señora Pelos" << endl;
+        cout << "Oro disponible: " << *oroActual << endl;
+        Arma* armaVenta = armas;
+
         for (int i = 0; i < cantidadArmas; i++) {
-            cout << i + 1 << "." << armasEnVenta[i].nombre << "-Precio: " << armasEnVenta[i].nombre << "-Daño: " << armasEnVenta[i].danio << endl;
+            cout << i + 1 << "." << armaVenta->nombre << "-" << armaVenta->precio << "oro" << endl;
+            armaVenta++;
         }
-        //Ver el menú :D
-        cout << "Opciones: " << endl;
-        cout << "¿Vas a comprar?" << endl;
-        cout << "¿Si o no?" << endl;
-        cout << "SI O NO";
-        int opcion;
+        cout << "Selecciona una opcion: " << endl;
+        cout << "1) Comprar Arma" << endl;
+        cout << "2) Salir de la tienda" << endl;
         cin >> opcion;
-
-        switch (opcion) {
-        case 1: { //Compra el arma ci
-            cout << "Llevele que va a comprar:";
-            int seleccion;
-            cin >> seleccion;
-
-            //Hora de la entrada
-            if (seleccion < 1 || seleccion> cantidadArmas) {
-                cout << "Por favor ese nooo" << endl;
-                break;
-            }
-            //Arma seleccionada
-            arma armaSeleccionada = armasEnVenta[seleccion - 1];
-            //Ver si el jugador tiene dinero
-            if (*oroActual >= armaSeleccionada.precio) {
-                *oroActual -= armaSeleccionada.precio;
-                cout << "Has comprado" << armaSeleccionada.nombre << "por" << armaSeleccionada.precio << "Moneditas de oro" << endl;
-            }
-            else {
-                cout << "Pobre no tienes dinero JAJAJA" << armaSeleccionada.nombre << "." << endl;
-            }
+        switch(opcion) {
+        case 1:
+            cout << "Seleccione un arma: " << endl;
             break;
-        }
         case 2:
-            cout << "Gracias por su visita en la tienda BYE BYE!!" << endl;
-            return 0;
-        defaut:
-            cout << "Ni modo no se pudo" << endl;
+            cout << "Saliste de la tienda" << endl;
         }
+        return 0;
     }
-    return 0;
+    
 }
